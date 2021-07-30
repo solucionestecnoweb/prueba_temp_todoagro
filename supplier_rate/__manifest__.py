dfd4eededd4edd78cb2e48f62fb76e0d8b439e77
spdev30 <87626858+spdev30@users.noreply.github.com>
2021-07-30 14:24:24 -0400

    Add files via upload
    Sirius

---
+++ supplier_rate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name":"Tasa de cambio del Proveedor",
+    "description":"Permite añadir la tasa de cambio de un proveedor.",
+    "author":"Oasis Consultora",
+    "depends":['contacts', 'account'],
+    "data":[
+        'views/views.xml',
+        'security/security.xml'
+    ]
+}
