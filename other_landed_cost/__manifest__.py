ad36aa621570e9db8c0d4aca7801484fec89de5d
ManuelNodal <87834800+ManuelNodal@users.noreply.github.com>
2021-08-27 18:54:56 -0400

    Add files via upload
    REF 08 De Compra y Venta

---
+++ other_landed_cost/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name":"Otros costos en Destino",
+    "descripcion":"Extension para añadir el campo otros costos en destino",
+     "depends":['product', 'l10n_ve_res_currency'],
+    "data":[
+        'views/inherit_views.xml'
+    ]
+}
