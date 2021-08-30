2a2fe3176df2aed874c802a1d277e9cd71712f66
spdev30 <87626858+spdev30@users.noreply.github.com>
2021-08-30 01:32:30 -0400

    RF006: Adecuaciones en el Libro de Ventas

---
+++ vat_sales_book/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name":"Libro de Ventas",
+    "description":"Añade la función de Libro de Ventas.",
+    "author":"Oasis Consultora",
+    "depends":['account', 'account_accountant'],
+    "data":[
+        'views/wizards_sales_book.xml',
+        'reports/sales_book.xml'
+    ]
+}
